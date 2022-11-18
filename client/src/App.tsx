40ca477842e501177acaabeb39ac2c6423e7c100
SebastianHinestroza12 <menasebastian1356@americana.edu.co>
2022-11-18 16:02:33 -0500

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavbarUser } from "./components/Navbar/index";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavbarUser />
+    </div>
+  );
 }
 
 export { App };
---
+++ client/src/components/Navbar/index.tsx
@@ -0,0 +1,36 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavbarUser() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Inicio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Navbar.Brand href="#">Created New User</Navbar.Brand>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export { NavbarUser };
